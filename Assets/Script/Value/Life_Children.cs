cf6546dc9fc8e79c6d962a6896662fda6f89bad5
shiokempi <ee24a055@oecu.jp>
2024-08-02 10:51:26 +0900

    スコア表示

---
+++ Assets/Script/Value/Life_Children.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life_Children : MonoBehaviour
+{
+    public Sprite[] m_sprite = new_Sprite[4];
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
