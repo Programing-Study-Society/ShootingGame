c2d60a75d88cfe02a217f7ee258bcd44663c1e62
Reioecu <poohayano0803@gmail.com>
2024-08-15 09:20:08 +0900

    コメント文

--- Assets/Script/move/enemySide.cs
+++ Assets/Script/move/enemySide.cs
@@ -6,7 +6,7 @@ public class enemySide : MonoBehaviour
 {
     private float MoveSpeed = 3.0f;
     
-    Void Update()
+    void FixedUpdate()
     {
         transform.position = new Vector3(Mathf.Sin(Time.time) * MoveSpeed, 0, 0);
     }
