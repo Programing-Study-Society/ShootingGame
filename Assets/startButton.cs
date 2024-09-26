2fe2f7f165cc5f525aa349173e83395fb0519163
Reioecu <poohayano0803@gmail.com>
2024-09-27 08:27:50 +0900

    コメント文

--- Assets/startButton.cs
+++ Assets/startButton.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 //ライブラリの追加
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class startButton : MonoBehaviour {  //始まった時に実行する関数　　void Start (){ //ボタンが押された時、StartGame関数を実行　gameObject.GetComponent<Button>().onClick.AddListener(StartaGame); } // StartGame関数　Void StartGame() { // GameSceneをロード　SceneManager.LoadScene("GameScene"); } 
+
+public class startButton : MonoBehaviour {  
+
+    //始まった時に実行する関数
+    void Start (){ 
+        //ボタンが押された時、StartGame関数を実行　
+        gameObject.GetComponent<Button>().onClick.AddListener(StartaGame); 
+    } 
+        
+        // StartGame関数　
+    Void StartGame() 
+    {
+         // GameSceneをロード
+         SceneManager.LoadScene("GameScene"); 
+    } 
 }
