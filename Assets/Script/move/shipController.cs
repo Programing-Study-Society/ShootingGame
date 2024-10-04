1404f0331cae368faa6aa3aac4c083506d48c22e
hayasi256 <linqingtailang9@gmail.com>
2024-10-04 17:42:39 +0900

    こうしん

--- Assets/Script/move/shipController.cs
+++ Assets/Script/move/shipController.cs
@@ -6,21 +6,24 @@ public class shipController : MonoBehaviour
 {
     public float speed;
     public Camera m_camera;
-    // Start is called before the first frame update
+
     void Start()
     {
-        m_camera = Camera.main;
+        m_camera = Camera.main;  // メインカメラを取得
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // マウスのスクリーン座標をワールド座標に変換
         var worldPoint = m_camera.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(worldPoint);
-        worldPoint.z = 0f;
-        transform.up = Vector3.MoveTowards(
-            transform.up,
-            worldPoint - transform.position,
-            10f * Time.deltaTime);
+        worldPoint.z = 0f;  // 2D空間として扱うため、z座標を0に固定
+
+        // キャラクターからマウスの方向へのベクトルを計算
+        Vector3 direction = worldPoint - transform.position;
+
+        // キャラクターの向きをマウスの方向に回転させる
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;  // ベクトルの角度を計算
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));  // 回転角度を設定
     }
 }
+
