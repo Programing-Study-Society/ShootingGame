8fd9a3b59d146198990b0afd2c7c84a05e9edb2f
hayasi256 <linqingtailang9@gmail.com>
2024-10-04 19:15:04 +0900

    こうしん

--- Assets/Script/move/shipController.cs
+++ Assets/Script/move/shipController.cs
@@ -1,29 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class shipController : MonoBehaviour
 {
-    public float speed;
-    public Camera m_camera;
+    float moveSpeed = 5;
+    [SerializeField] Rigidbody2D rb;
 
-    void Start()
-    {
-        m_camera = Camera.main;  // メインカメラを取得
-    }
+    Vector2 movement;
+    Vector2 mousePos;
+    //カメラの参照
+    [SerializeField] Camera camera;
 
     void Update()
     {
-        // マウスのスクリーン座標をワールド座標に変換
-        var worldPoint = m_camera.ScreenToWorldPoint(Input.mousePosition);
-        worldPoint.z = 0f;  // 2D空間として扱うため、z座標を0に固定
+        movement.x = Input.GetAxisRaw("Horizontal");
 
-        // キャラクターからマウスの方向へのベクトルを計算
-        Vector3 direction = worldPoint - transform.position;
+        movement.y = Input.GetAxisRaw("Vertical");
+        //マウスの位置を取得
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
 
-        // キャラクターの向きをマウスの方向に回転させる
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;  // ベクトルの角度を計算
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));  // 回転角度を設定
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //マウスの向きを取得
+        Vector2 lookDir = mousePos - rb.position;
+        //マウスの角度を取得する
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
-
