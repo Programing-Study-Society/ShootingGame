bbbc1e0342724b7f9709b2a013989cfd42624745
Reioecu <poohayano0803@gmail.com>
2024-10-30 19:02:38 +0900

    エネミー移動修正

--- Assets/startButton.cs
+++ Assets/startButton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //ライブラリの追加
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
+
 public class startButton : MonoBehaviour {  
     //始まった時に実行する関数　　
     void Start (){ 
@@ -24,21 +24,3 @@ public class startButton : MonoBehaviour {
         SceneManager.LoadScene("GameScene"); 
     } 
 }
-=======
-
-public class startButton : MonoBehaviour {  
-
-    //始まった時に実行する関数
-    void Start (){ 
-        //ボタンが押された時、StartGame関数を実行　
-        gameObject.GetComponent<Button>().onClick.AddListener(StartaGame); 
-    } 
-        
-        // StartGame関数　
-    Void StartGame() 
-    {
-         // GameSceneをロード
-         SceneManager.LoadScene("GameScene"); 
-    } 
-}
->>>>>>> 2fe2f7f165cc5f525aa349173e83395fb0519163
