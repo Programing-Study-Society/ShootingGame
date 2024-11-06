344f2f7d5883114b499277feab4af9d5de8df5c1
shiokempi <ee24a055@oecu.jp>
2024-11-06 20:01:50 +0900

    スコアボード途中

---
+++ Assets/Script/ScoreBoard/ScoreBoard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+    //GlovalValue.ScoreList;
+
+public class ScoreBoard : MonoBehaviour
+{
+    public Text score_text;
+
+    public int StageSelect;
+
+    public void OnClick()
+    {
+        score_text.text = GlovalValue.ScoreList[StageSelect-1,0].ToString()+"\n\n";
+        score_text.text += GlovalValue.ScoreList[StageSelect-1,1].ToString()+"\n\n";
+        score_text.text += GlovalValue.ScoreList[StageSelect-1,2].ToString()+"\n\n";
+        score_text.text += GlovalValue.ScoreList[StageSelect-1,3].ToString()+"\n\n";
+        score_text.text += GlovalValue.ScoreList[StageSelect-1,4].ToString();
+
+        //ScoreList[0,0]
+
+    }
+}
--- Assets/Script/ScoreBoard/popup.cs
+++ Assets/Script/ScoreBoard/popup.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class score : MonoBehaviour
-{
-    public GameObject score_object;
-    Text score_text;
 
+public class popup : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
-        //GetCompornentはstartへ
-        score_text = score_object.GetComponent<Text> ();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        score_text.text = GlovalValue.score.ToString();
+        
+    }
+
+    void OnClick()
+    {
+
     }
 }
